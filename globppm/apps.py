b375ee3872f4d63dbafbee583624fdbfb221860b
ricardosallin <ricardo.sallin@gmail.com>
2016-12-21 22:40:56 -0200

    Changed the HTML for the site.

--- globppm/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GlobppmConfig(AppConfig):
-    name = 'globppm'
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'blog',
-	'globppm',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -108,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = '	America/Sao_Paulo'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
